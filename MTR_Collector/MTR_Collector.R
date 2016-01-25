15da347ddae5e51689358acb0fd465fad77ec060
IFFranciscoME <if.francisco.me@gmail.com>
2016-01-24 21:58:05 -0600

    Collector y Tokens

--- MTR_Collector/MTR_Collector.R
+++ MTR_Collector/MTR_Collector.R
@@ -7,10 +7,7 @@
 
 # -- Cargar librerias ---------------------------------------------------------------- #
 
-pkg <- c("base","downloader","fBasics","forecast","grid","gridExtra",
-         "httr","jsonlite","lubridate","moments","PerformanceAnalytics","plyr",
-         "quantmod","reshape2","RCurl","stats","scales","tree","tseries",
-         "TTR","TSA","xts","xlsx","zoo","twitteR","tm")
+pkg <- c("base","downloader","httr","jsonlite","quantmod","RCurl","xts","twitteR")
 
 inst <- pkg %in% installed.packages()
 if(length(pkg[!inst]) > 0) install.packages(pkg[!inst])
@@ -27,15 +24,15 @@ downloader::source_url(ROandaAPI,prompt=FALSE,quiet=TRUE)
 OnTokens <- read.csv("~/Documents/GitHub/MachineTradeR/MTR_Tokens/OandaTokens.csv")
 
 ONAT <- as.character(OnTokens[1,1])  # Account Type
-ONAI <- as.numeric(OnTokens[2,1])    # Account ID
-ONAK <- as.character(OnTokens[3,1])  # Account Token
+ONAI <- as.numeric(OnTokens[1,2])    # Account ID
+ONAK <- as.character(OnTokens[1,3])  # Account Token
 
 TwTokens <- read.csv("~/Documents/GitHub/MachineTradeR/MTR_Tokens/TwitterTokens.csv")
 
-TWCK <- as.character(TwTokens[2,1])  # Consumer Key
-TWCS <- as.character(TwTokens[2,2])  # Consumer Secret
-TWAT <- as.character(TwTokens[2,3])  # Access Token
-TWAS <- as.character(TwTokens[2,4])  # Access Token Secret
+TWCK <- as.character(TwTokens[1,1])  # Consumer Key
+TWCS <- as.character(TwTokens[1,2])  # Consumer Secret
+TWAT <- as.character(TwTokens[1,3])  # Access Token
+TWAS <- as.character(TwTokens[1,4])  # Access Token Secret
 
 # -- Precios iniciales --------------------------------------------------------------- #
 
@@ -44,10 +41,12 @@ Fin <- Sys.Date()      # Fecha Final
 
 ON_Gt <- "D"        # Granularity: Frecuencia de muestre de precio.
 ON_In <- "USD_MXN"  # Instrument: Instrumento Financiero a utilizar.
+ON_Da <- 17
+ON_Ta <- "America%2FMexico_City" # Uso horario
 
 # Lista de instrumentos disponibles
-ON_Ls <- data.frame(InstrumentsList(AccountType,Token,AccountID))[,c(1,3)]
+ON_Ls <- data.frame(InstrumentsList(ONAT,ONAK,ONAI))[,c(1,3)]
 # Peticion de precios historicos
-ON_Ph <- HisPrices(AccountType,Granularity,DayAlign,TimeAlign,Token,TInstrument,Ini,Fin)
+ON_Ph <- HisPrices(ONAT,ON_Gt,ON_Da,ON_Ta,ONAK,ON_In,Ini,Fin)
 # Peticion de precio actual
-ON_Pa <- ActualPrice(AccountType,Token,TInstrument)
+ON_Pa <- ActualPrice(ONAT,ONAK,ON_In)
