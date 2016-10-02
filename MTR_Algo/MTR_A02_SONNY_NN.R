c6be24d0bde27c4444ad82438e2d51a093622a80
ENVY\IFFranciscoME <if.francisco.me@gmail.com>
2016-10-02 17:08:41 -0500

    Actualizaciones basicas

--- MTR_Algo/MTR_A02_SONNY_NN.R
+++ MTR_Algo/MTR_A02_SONNY_NN.R
@@ -7,9 +7,10 @@
 
 # -- Cargar Datos Necesarios --------------------------------------------------------- #
 
+Datos_A02 <- A02_PreciosHis
+Datos_A02 <- Datos_A02[,1:5]
 
-
-ACTIVO <- xts(ON_PhD[,2:5], order.by = ON_PhD[,1])
+Datos_A02 <- xts(Datos_A02[,2:5], order.by = Datos_A02[,1])
 
 # -- Construccion de variables con analisis tecnico para Modelo ---------------------- #
 
@@ -27,15 +28,15 @@ myEMA60 <- function(x) EMA(Cl(x),n=60)[,1] # Exponential Moving Average 60 perio
 myEMA70 <- function(x) EMA(Cl(x),n=70)[,1] # Exponential Moving Average 70 periods
 mySAR   <- function(x) SAR(x[,c('High','Close')]) [,1] # Parabolic Stop-and-Reverse
 
-data.model <- specifyModel(Delt(Cl(ACTIVO)) ~ 
-                             myATR(ACTIVO) + mySMI(ACTIVO) + myADX(ACTIVO) + 
-                             myBB(ACTIVO)  + myMACD(ACTIVO) + myEMA10(ACTIVO) + 
-                             myEMA20(ACTIVO) + myEMA30(ACTIVO) + myEMA40(ACTIVO) + 
-                             myEMA50(ACTIVO) + myEMA60(ACTIVO) + myEMA70(ACTIVO) + 
-                             mySAR(ACTIVO) + EMA(Delt(Cl(ACTIVO))) + runSD(Cl(ACTIVO)) + 
-                             RSI(Cl(ACTIVO), n=5)  + RSI(Cl(ACTIVO), n=10) + 
-                             RSI(Cl(ACTIVO), n=15) + RSI(Cl(ACTIVO), n=20) + 
-                             RSI(Cl(ACTIVO), n=25) + RSI(Cl(ACTIVO), n=30))
+data.model <- specifyModel(Delt(Cl(Datos_A02)) ~ 
+                             myATR(Datos_A02) + mySMI(Datos_A02) + myADX(Datos_A02) + 
+                             myBB(Datos_A02)  + myMACD(Datos_A02) + myEMA10(Datos_A02) + 
+                             myEMA20(Datos_A02) + myEMA30(Datos_A02) + myEMA40(Datos_A02) + 
+                             myEMA50(Datos_A02) + myEMA60(Datos_A02) + myEMA70(Datos_A02) + 
+                             mySAR(Datos_A02) + EMA(Delt(Cl(Datos_A02))) + runSD(Cl(Datos_A02)) + 
+                             RSI(Cl(Datos_A02), n=5)  + RSI(Cl(Datos_A02), n=10) + 
+                             RSI(Cl(Datos_A02), n=15) + RSI(Cl(Datos_A02), n=20) + 
+                             RSI(Cl(Datos_A02), n=25) + RSI(Cl(Datos_A02), n=30))
 
 
 DatoModelo <- as.data.frame(modelData(data.model))
@@ -47,26 +48,21 @@ signals <- function(x) {
     if(x <  0) {resultado <- "sell"}
   resultado }
 
-Clase <- sapply(DatoModelo$Delt.Cl.ACTIVO, signals)
+Clase <- sapply(DatoModelo$Delt.Cl.Datos_A02, signals)
 traindata <- cbind(DatoModelo,Clase)
 testdata  <- data.frame(DatoModelo[length(DatoModelo[,1]),])
-traindata$Delt.Cl.ACTIVO <- NULL
+traindata$Delt.Cl.Datos_A02 <- NULL
 
 # -- ------------------------------------------------- Configuracion Cluster de H2O -- #
 
-#h2o.init(nthreads=-1, max_mem_size='7G')
-traindata <- as.h2o(traindata)
-testdata <- as.h2o(testdata)
+h2o.init(nthreads=-1, max_mem_size='1G')
+# traindata <- as.h2o(traindata)
+# testdata <- as.h2o(testdata)
 
 # -- ----------------------------------------------------- Machine Learning Methods -- #
 
-
-
-Datos <- data.frame(ResagosCl$TimeStamp[-1],ifelse(diff(log(ResagosCl$PrecioCl))>0, 1,0))
-Predictores <- colnames(ResagosCl)[3:8]
-
 DeepNN <- h2o.deeplearning(x = Predictores, y = "Clase", training_frame = traindata,
-                           validation_frame = traindata, activation = "Tanh",
+                           validation_frame = testdata, activation = "Tanh",
                            hidden = c(100,100), loss = "CrossEntropy",
                            diagnostics = TRUE)
 
--- MTR_Collector/MTR_Collector.R
+++ MTR_Collector/MTR_Collector.R
@@ -17,6 +17,8 @@ A01_FechaFin    <- Sys.Date()
 A01_PreciosHis  <- HisPrices(OA_At,"H4",OA_Da,OA_Ta,OA_Ak,"WTICO_USD",NULL,NULL,200)
 A01H_PreciosHis <- HisPrices(OA_At,"H4",OA_Da,OA_Ta,OA_Ak,"EUR_USD",NULL,NULL,200)
 
+A02_PreciosHis <- HisPrices(OA_At,"H1",OA_Da,OA_Ta,OA_Ak,"AUD_USD",NULL,NULL,200)
+
 A01_PreciosAct  <- list("FT_CL-Nov!!" = TP_GetSymbol("FT_CL-Nov!!")[3:4])
 A01H_PreciosAct <- list("EURUSD" = TP_GetSymbol("EURUSD")[3:4])
 
