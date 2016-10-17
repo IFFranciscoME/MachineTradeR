430d162e88ffa71c3f74a34a322c0d9d5e016996
root <root@ip-172-31-15-3.us-west-1.compute.internal>
2016-10-17 01:27:15 +0000

    Modificaciones para AWS

--- MTR_Control/MTR_Control_H4.R
+++ MTR_Control/MTR_Control_H4.R
@@ -1,22 +1,19 @@
 
-# ------------------------------------------------------------------------------------ #
-# -- Initial Developer: FranciscoME ----------------------------------------------- -- #
-# -- Code: MachineTradeR Main Control H4 ------------------------------------------ -- #
-# -- License: TradingPal ---------------------------------------------------------- -- #
-# ------------------------------------------------------------------------------------ #
+# ------------------------------------------------------------------------------------- #
+# -- Initial Developer: FranciscoME ------------------------------------------------ -- #
+# -- Code: MachineTradeR Main Control H4 ------------------------------------------- -- #
+# -- License: TradingPal ----------------------------------------------------------- -- #
+# ------------------------------------------------------------------------------------- #
 
 # -- Borrar todos los elementos del environment
 rm(list=ls())
 
-# -- ETAPA 0 ---------------------------------------------------------------------- -- #
-# -- Inicializador general de sistema ------------------------------ MTR_Control_H4 -- #
-# -- ------------------------------------------------------------------------------ -- #
+# -- ETAPA 0 ----------------------------------------------------------------------- -- #
+# -- Inicializador general de sistema ------------------------------- MTR_Control_H4 -- #
+# -- ------------------------------------------------------------------------------- -- #
 
 # -- Establecer el sistema de medicion de la computadora
 Sys.setlocale(category = "LC_ALL", locale = "")
-
-# -- Horas admitidas para hacer correr cada 4 Horas los codigos
-Horas_H4 <- c(2,6,10,14,18,22)
 
 # -- Paquetes o librerias utilizadas
 pkg <- c("base","downloader","dplyr","fBasics","forecast","googlesheets","grid",
@@ -41,51 +38,37 @@ downloader::source_url(RTradingPal,prompt=FALSE,quiet=TRUE)
 
 # -- ETAPA 1 ----------------------------------------------------------------------- -- #
 # -- Informacion de cuentas a utilizar -------------------------------- MTR_Registro -- #
+# -- ------------------------------------------------------------------------------- -- #
 
-source('C:/TradingPal/BitBucket/R_MachineTradeR/MTR_Registro/MTR_Registro.R', echo=TRUE)
+source('~/r_machinetrader/MTR_Registro/MTR_Registro.R', echo=TRUE)
 
 # -- ETAPA 2 ----------------------------------------------------------------------- -- #
 # -- Recolectar Datos para uso en Sistema ---------------------------- MTR_Collector -- #
+# -- ------------------------------------------------------------------------------- -- #
 
-source('C:/TradingPal/BitBucket/R_MachineTradeR/MTR_Collector/MTR_Collector.R', echo=TRUE)
+source('~/r_machinetrader/MTR_Collector/MTR_Collector.R', echo=TRUE)
 
 # -- ETAPA 3 ----------------------------------------------------------------------- -- #
 # -- Ejecutar Algoritmos para generacion de senales ---------------------- MTR_Algos -- #
+# -- ------------------------------------------------------------------------------- -- #
 
-Hora <- lubridate::hour(as.POSIXct(Sys.timeDate(), origin = "1970-01-01",
-                                   tz = "America/Mexico_City"))
-
-if(any(c(Hora == Horas_H4))) {
-  A01_Bandera <- 1
-
-source('C:/TradingPal/BitBucket/R_MachineTrader/MTR_Algo/MTR_A01_PELHAM_BJ.R', echo=TRUE)
-source('C:/TradingPal/BitBucket/R_MachineTradeR/MTR_Algo/MTR_A01_PELHAM_BJ_Hedge.R', echo=TRUE)
-
-} else {
-
-  A01_Bandera <- 0
-  print("                                                  ")
-  print("--------------------------------------------------")
-  print("A01 En Espera, Periodicidad de tiempo no alcanzada")
-  print("--------------------------------------------------")
-  print("                                                  ")
-  stop()
-
-}
+source('~/r_machinetrader/MTR_Algo/MTR_A01_PELHAM_BJ.R', echo=TRUE)
+source('~/r_machinetrader/MTR_Algo/MTR_A01_PELHAM_BJ_Hedge.R', echo=TRUE)
 
 # -- ETAPA 5 ----------------------------------------------------------------------- -- #
 # -- Colocar operaciones con parametros generados por Algoritmos -------- MTR_Trader -- #
+# -- ------------------------------------------------------------------------------- -- #
 
-source('C:/TradingPal/BitBucket/R_MachineTradeR/MTR_Trader/MTR_Trader_TP.R', echo=TRUE)
-#source('C:/TradingPal/BitBucket/R_MachineTradeR/MTR_Trader/MTR_Trader_OA.R', echo=TRUE)
+source('~/r_machinetrader/MTR_Trader/MTR_Trader_TP.R', echo=TRUE)
 
 # -- ETAPA 4 ----------------------------------------------------------------------- -- #
 # -- Generar y Enviar senal a traves de SMS y Email ------------------- MTR_Notifier -- #
+# -- ------------------------------------------------------------------------------- -- #
 
-source('C:/TradingPal/BitBucket/R_MachineTradeR/MTR_Notifier/MTR_Notifier.R', echo=TRUE)
+source('~/r_machinetrader/MTR_Notifier/MTR_Notifier.R', echo=TRUE)
 
 # -- ETAPA 6 ----------------------------------------------------------------------- -- #
 # -- Publicar en Trading Pal y Twitter resultados -------------------- MTR_Publisher -- #
 # -- ------------------------------------------------------------------------------- -- #
 
-source('C:/TradingPal/BitBucket/R_MachineTradeR/MTR_Publisher/MTR_Publisher.R', echo=TRUE)
+source('~/r_machinetrader/MTR_Publisher/MTR_Publisher.r', echo=TRUE)
