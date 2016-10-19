d96c5dfcfe7dfe2090f82c23aee3d8e095d5a9f5
ENVY\IFFranciscoME <sergio.hurtado@udem.edu>
2016-10-19 15:59:59 -0500

    Cambios en Envy

--- MTR_Trader/MTR_Trader_TP.R
+++ MTR_Trader/MTR_Trader_TP.R
@@ -6,18 +6,6 @@
 # ------------------------------------------------------------------------------------- #
 
 # ------------------------------------------------------------------------------------- #
-# -- Detectar Actividad de Algoritmos ------------------------------------------------- #
-# ------------------------------------------------------------------------------------- #
-
-# -- Escenarios
-# -- 1.- Ninguna operacion abierta 
-# -- 1.1.- Proceder a abrir ops enviadas por MTR_Algo
-
-# -- 2.- Localizar operaciones abiertas del mismo instrumento enviado por MTR_Algo 
-# -- 2.1.- Proceder a cerrar todas las ops abiertas
-# -- 2.2.- Proceder a abrir ops enviadas por MTR_Algo
-
-# ------------------------------------------------------------------------------------- #
 # -- Trading para A01H_PELHAM_BJ_Hedge ------------------------------------------------ #
 # ------------------------------------------------------------------------------------- #
 
@@ -152,31 +140,3 @@ if(length(A01_PELHAM_BJ$DatosTrade) != 0){
       P5_op_type = A01_PELHAM_BJ$DatosTrade$Trade)  
   }
 }
-
-# ------------------------------------------------------------------------------------- #
-# -- Trading para A03_ROBBY_RF -------------------------------------------------------- #
-# ------------------------------------------------------------------------------------- #
-
-# # -- Conocer si hay operaciones abiertas
-# A03_ROBBY_RF_OpenTrades  <- TP_GetTrades(A03_ROBBY_RF$TPUID)
-# 
-# TradeROBBY <- OpenTrade(P0_Token = as.character(ROBBY$Token$Token),
-#                         P1_symbol = "AUDUSD",
-#                         P2_sl = 1,
-#                         P3_tp = 2,
-#                         P4_lots = 0.5,
-#                         P5_op_type = "buy")
-# 
-# # ------------------------------------------------------------------------------------- #
-# # -- Trading para A04_BENDER_LR ------------------------------------------------------- #
-# # ------------------------------------------------------------------------------------- #
-# 
-# A04_BENDER_LR_OpenTrades <- TP_GetTrades(A04_BENDER_LR$TPUID)
-#  
-# TradeBENDER <- OpenTrade(P0_Token = as.character(BENDER$Token$Token),
-#                          P1_symbol = "NZDUSD",
-#                          P2_sl = 0.5 ,
-#                          P3_tp = 1,
-#                          P4_lots = 0.1,
-#                          P5_op_type = "sell")
-#  
